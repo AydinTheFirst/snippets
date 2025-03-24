316bbe53377fb1f57fe4df23bb85dbab672ecbf4
Halil Aydın <84444757+AydinTheFirst@users.noreply.github.com>
2025-03-24 22:19:06 +0300

    Update index.ts

--- ts/http/index.ts
+++ ts/http/index.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { toast } from "sonner";
 
-import { sleep } from "@/utils";
+import { sleep } from "@/lib/utils";
 
 const baseURL = import.meta.env.VITE_API_URL || "/api";
 
